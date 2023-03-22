168f16998854fed8594753aee6fa0d1a1580e16f
sshivahh <russelshivah16@gmail.com>
2023-03-22 12:15:42 +0700

    edit sesuai figma

--- pages/js/main.js
+++ pages/js/main.js
@@ -1,15 +1,36 @@
 function openNew(){
-    $("#popupNew").width("40vh");
-    $("#popupNew").height("50vh");
+    $("#popupNew").width("60vh");
+    $("#popupNew").height("60vh");
 
+    $(".menuBtn").width(0);
+    $(".menuBtn").height(0);
 }
 
 function closeNew(){
     $("#popupNew").width(0);
     $("#popupNew").height(0);
 
+    $(".menuBtn").width("150px");
+    $(".menuBtn").height("50px");
+}
+
+function openCred(){
+    $("#popupCred").width("60vh");
+    $("#popupCred").height("50vh");
+
+    $(".menuBtn").width(0);
+    $(".menuBtn").height(0);
+}
+
+function closeCred(){
+    $("#popupCred").width(0);
+    $("#popupCred").height(0);
+
+    $(".menuBtn").width("150px");
+    $(".menuBtn").height("50px"); 
 }
 
 function startGame(){
     window.location.href = "game-lobby.html";
 }
+
